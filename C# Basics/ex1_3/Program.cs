8a504b7485d721aef2beb0306e99cc0ec9e5a4ab
DemydKorotkykh <d.korot.che@gmail.com>
2023-01-12 22:05:36 +0100

    ex1_3 added

---
+++ C# Basics/ex1_3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        int width = 0;
+        int height = 0;
+        
+            Console.WriteLine("Enter width of image: ");
+            width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter height of image: ");
+            height = Convert.ToInt32(Console.ReadLine());
+        if (width > height)
+        {
+            Console.WriteLine("Landscape");
+
+        }
+        else
+        {
+            Console.WriteLine("Portrate");
+        }   
+
+           
+    }
+            
+}
+
+
