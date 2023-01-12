465931ae60ebaec059e1c405afa15e7006b19217
DemydKorotkykh <d.korot.che@gmail.com>
2023-01-12 22:29:20 +0100

    ex1_4 was added

---
+++ C# Basics/ex1_4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        int limit = 0;
+        int car = 0;
+        int count = 0;
+            Console.WriteLine("Enter the speed limit: ");
+            limit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the speed of a car: ");
+            car = Convert.ToInt32(Console.ReadLine());
+            count = car - limit;
+        if (limit > car)
+        {
+            Console.WriteLine("Ok");
+
+        }
+        else
+        {
+            if( count >= 60)
+            {
+                Console.WriteLine("License Suspended");
+            }
+            else
+            {
+                count = count / 5;
+                Console.WriteLine("{0} demerit points", count);
+            }
+        }   
+
+           
+    }
+            
+}
+
+
