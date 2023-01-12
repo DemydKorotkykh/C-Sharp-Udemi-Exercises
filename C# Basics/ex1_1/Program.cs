a611cdf3ed63651841d1205e319c682e4a4277b6
DemydKorotkykh <d.korot.che@gmail.com>
2023-01-12 19:56:03 +0100

    Added ex 1_1 and 1_2

---
+++ C# Basics/ex1_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        int input = 0;
+        
+            Console.WriteLine("Enter a number: ");
+            input = Convert.ToInt32(Console.ReadLine());
+        if (input >= 1 && input <= 10)
+        {
+            Console.WriteLine("Vallid ");
+
+        }
+        else
+        {
+            Console.WriteLine("Invalid");
+        }   
+
+           
+    }
+            
+}
+
---
+++ C# Basics/ex1_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        int a = 0;
+        int b = 0;
+        
+            Console.WriteLine("Enter first number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+        if (a > b)
+        {
+            Console.WriteLine( a );
+
+        }
+        else
+        {
+            Console.WriteLine( b );
+        }   
+
+           
+    }
+            
+}
+
+
