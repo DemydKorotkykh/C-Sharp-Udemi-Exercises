8b62fce515b1e5b1833de6d0228da6ec5e85a7b0
DemydKorotkykh <d.korot.che@gmail.com>
2023-01-14 15:06:33 +0100

    ex2_1 added

---
+++ C# Basics/ex2_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int count = 0;
+        for(int i = 1; i <= 100; i++)
+        {
+            if ((i % 3) == 0)
+            {
+                count++;
+            }
+
+        }
+        Console.WriteLine("In range from 1 to 100 is {0} numbers divadable by 3", count);
+    }        
+}
