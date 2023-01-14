3f2a1592ecdd91c412bcbd33617db59a09a26476
DemydKorotkykh <d.korot.che@gmail.com>
2023-01-14 15:44:48 +0100

    ex2_2 added

---
+++ C# Basics/ex2_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        //int input;
+        int count = 0;
+        int count2 = 0;
+        while (true)
+        {
+            Console.WriteLine("Enter a number to continue or 'ok' to end program");
+            var input = Console.ReadLine();
+            bool result = int.TryParse(input, out count);
+
+            if (result != true)
+            {
+                switch (input)
+                {
+                    case "ok":
+                        Console.WriteLine("Symmarisee {0}", count2);
+                        break;
+
+                    default:
+                        continue;
+
+                }
+                break;
+            }
+            else
+            {
+                count2 = count2 + count;
+                continue;
+            }
+        }
+    }
+}
