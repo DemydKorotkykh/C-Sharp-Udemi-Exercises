f97eda6b74702b6f3eefb98ee8baf712dcee6bfc
DemydKorotkykh <d.korot.che@gmail.com>
2023-01-14 16:01:21 +0100

    created ex2_3

--- C# Basics/ex2_3/Program.cs
+++ C# Basics/ex2_3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+            Console.WriteLine("Enter a number to calculate factorial:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int count = input;
+            for(int i = input - 1; i > 0; i--)
+            {
+                count = count * i;
+            }
+
+         Console.WriteLine("Symmarisee {0}! = {1}", input, count);
+            
+            
+            
+    }
+}
