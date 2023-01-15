f7e957386d8487b1823a69ec137c6bf7272cfb9a
DemydKorotkykh <d.korot.che@gmail.com>
2023-01-15 15:30:51 +0100

    added ex2_4

---
+++ C# Basics/ex2_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Random rnd = new Random();
+        int random = new Random().Next(1, 10);
+        Console.WriteLine("Guess number");
+        for (int i = 4; i > 0; i--)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            if (input == random)
+            {
+                Console.WriteLine("You won");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("You lost");
+                continue;
+            }
+        }
+    }
+}
