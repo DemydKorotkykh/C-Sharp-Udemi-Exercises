18cc2649967ddd46261e3ee47dae95f762590e68
DemydKorotkykh <d.korot.che@gmail.com>
2023-01-22 21:07:02 +0100

    Added ex3_3

---
+++ C# Basics/ex3_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var numbers = new int[5];
+        while (true)
+        {
+            Console.WriteLine("Enter 5 digits: ");
+            for (int i = 0; i != 5; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            if (numbers.Length != numbers.Distinct().Count())
+            {
+                Console.WriteLine("Try again, do not use same digit twice");
+                continue;
+            }
+            else
+            {
+                Array.Sort(numbers);
+                Console.WriteLine();
+                Console.WriteLine("Results after sorting");
+                foreach (var n in numbers)
+                {
+                    Console.WriteLine(n);
+                }
+                break;
+            }
+        }
+    }
+}
