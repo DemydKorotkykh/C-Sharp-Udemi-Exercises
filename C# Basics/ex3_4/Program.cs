beb627d08174bdbce9f314f8a8a9b6eda9deff80
DemydKorotkykh <d.korot.che@gmail.com>
2023-01-22 21:35:19 +0100

    added ex3_4

---
+++ C# Basics/ex3_4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        
+        var count = new  List <int>();
+        var junk = 0;
+            Console.WriteLine("Enter a number to continue or 'Quit' to end program");
+        while (true)
+        {
+                var input = Console.ReadLine();
+                bool result = int.TryParse(input, out junk);
+
+            if (result != true)
+            {
+                switch (input)
+                {
+                    case "Quit":
+                    Console.WriteLine();
+                    foreach(var n in count)
+                    {
+                        Console.WriteLine(n);
+                    }
+                        break;
+
+                    default:
+                        continue;
+
+                }
+                break;
+            }
+            else
+            {
+                count.Add(junk);
+                continue;
+            }
+            
+        }
+    }
+}
