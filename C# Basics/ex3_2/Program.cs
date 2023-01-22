c35f40fb2609c5942f6f6eecfdebe562463153a7
DemydKorotkykh <d.korot.che@gmail.com>
2023-01-22 20:42:23 +0100

    Added ex3_2

---
+++ C# Basics/ex3_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Enter Your name: ");
+        string usrinp = Console.ReadLine();
+        char[] lettres = usrinp.ToCharArray();
+        Array.Reverse(lettres);
+        string reverse = new string (lettres);
+        Console.WriteLine(reverse);
+    }
+}
